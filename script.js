9a2ba34f21d25ef4045b38dfa4918ed1edc72565
aladores <angelo.ladores@outlook.com>
2023-11-08 21:01:48 -0500

    add header and change blur to css instead of javascript

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const movieListItems = document.querySelectorAll(".movie-preview-list-item");
+const movieListLinks = document.querySelectorAll(".movie-preview-list-item");
 const movieListImages = document.querySelectorAll(".movie-preview-image");
 
+for (let i = 0; i < movieListLinks.length; i++) {
+    movieListLinks[i].addEventListener("mouseover", () => {
+        changeFeatured(i);
+    })
+}
 
-function changeFeatured(moveListItem, position) {
+function changeFeatured(position) {
 
     //Current image
     movieListImages[position].classList.add("show");
@@ -12,43 +17,29 @@ function changeFeatured(moveListItem, position) {
     movieListImages.forEach((item, index) => {
         if (index !== position) {
             item.classList.remove("show");
-
             item.classList.add("hidden");
         }
     })
 
-    // Gradually remove the blur from the current image
-    let blurAmount = 5;
-    const blurInterval = setInterval(() => {
-        blurAmount -= 0.5; // Adjust the speed of the blur removal
-        movieListImages[position].style.filter = `blur(${blurAmount}px)`;
-
-        if (blurAmount <= 0) {
-            clearInterval(blurInterval);
-            movieListImages[position].style.filter = "blur(0)";
+    //Current Link
+    movieListLinks[position].classList.add("active");
+    //Previous images
+    movieListLinks.forEach((item, index) => {
+        if (index !== position) {
+            item.classList.remove("active");
         }
-    }, 100); // Adjust the interval and speed as needed
-}
-
-// function changeFeatured(position) {
-//     const previousImage = document.querySelector(".movie-preview-image.show");
-//     const currentImage = movieListImages[position];
-
-//     // Apply blur to the newly shown image
-//     currentImage.style.filter = "blur(5px)";
-//     currentImage.classList.add("show");
-//     currentImage.classList.remove("hidden");
-
-//     if (previousImage) {
-//         // Remove blur from the previously shown image
-
-//         previousImage.style.filter = "blur(0)";
-//         previousImage.classList.remove("show");
-//         previousImage.classList.add("hidden");
-//     }
-// }
-for (let i = 0; i < movieListItems.length; i++) {
-    movieListItems[i].addEventListener("mouseover", () => {
-        changeFeatured(movieListItems[i], i);
     })
+
+    // // Gradually remove the blur from the current image
+    // let blurAmount = 5;
+    // const blurInterval = setInterval(() => {
+    //     blurAmount -= 0.5; // Adjust the speed of the blur removal
+    //     movieListImages[position].style.filter = `blur(${blurAmount}px)`;
+
+    //     if (blurAmount <= 0) {
+    //         clearInterval(blurInterval);
+    //         movieListImages[position].style.filter = "blur(0)";
+    //     }
+    // }, 100); // Adjust the interval and speed as needed
 }
+
