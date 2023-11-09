2ef59ab7490b50efa5081b3fade363e019113705
aladores <angelo.ladores@outlook.com>
2023-11-08 21:26:50 -0500

    optimize javascript

--- script.js
+++ script.js
@@ -1,45 +1,39 @@
 const movieListLinks = document.querySelectorAll(".movie-preview-list-item");
 const movieListImages = document.querySelectorAll(".movie-preview-image");
+let index = 0;
+let intervalId;
 
+function changeFeatured(position) {
+    // Hide current image 
+    movieListImages[index].classList.remove("show");
+    movieListLinks[index].classList.remove("active");
+    movieListImages[index].classList.add("hidden");
+
+    // Show selected image
+    movieListImages[position].classList.add("show");
+    movieListLinks[position].classList.add("active");
+    movieListImages[position].classList.remove("hidden");
+
+    index = position;
+}
+
+function autoChangeImage() {
+    const nextIndex = (index + 1) % movieListLinks.length;
+    changeFeatured(nextIndex);
+}
+
+function startInterval() {
+    clearInterval(intervalId);
+    intervalId = setInterval(autoChangeImage, 4000);
+}
+
+//Start the slideshow
+startInterval();
+
+// Manually selected image via links
 for (let i = 0; i < movieListLinks.length; i++) {
     movieListLinks[i].addEventListener("mouseover", () => {
         changeFeatured(i);
-    })
+        startInterval();
+    });
 }
-
-function changeFeatured(position) {
-
-    //Current image
-    movieListImages[position].classList.add("show");
-    movieListImages[position].classList.remove("hidden");
-
-    //Previous images
-    movieListImages.forEach((item, index) => {
-        if (index !== position) {
-            item.classList.remove("show");
-            item.classList.add("hidden");
-        }
-    })
-
-    //Current Link
-    movieListLinks[position].classList.add("active");
-    //Previous images
-    movieListLinks.forEach((item, index) => {
-        if (index !== position) {
-            item.classList.remove("active");
-        }
-    })
-
-    // // Gradually remove the blur from the current image
-    // let blurAmount = 5;
-    // const blurInterval = setInterval(() => {
-    //     blurAmount -= 0.5; // Adjust the speed of the blur removal
-    //     movieListImages[position].style.filter = `blur(${blurAmount}px)`;
-
-    //     if (blurAmount <= 0) {
-    //         clearInterval(blurInterval);
-    //         movieListImages[position].style.filter = "blur(0)";
-    //     }
-    // }, 100); // Adjust the interval and speed as needed
-}
-
